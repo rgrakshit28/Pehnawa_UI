83caa114da183f0cfc506c0c5e858d81d59fd2af
rgrakshit28 <rgrakshit28.professional@gmail.com>
2023-04-15 11:40:48 +0530

    UI setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Phenawa</h1>
+      <h2>Buy or Rent your dream clothes</h2>
     </div>
   );
 }
